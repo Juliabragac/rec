47cb490c2e0419a3e3881c6f53be203c43d6acda
Julia Braga <bragajuliacorr1@gmail.com>
2022-03-07 16:55:01 -0300

    segundo repositorio

--- php/conexao.php
+++ php/conexao.php
@@ -9,3 +9,9 @@ try {
 catch (Exception $th) {
     echo "Erro com o banco de dados: " . $th;
 }
+
+try {
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (\Throwable $th) {
+    echo "Erro: " . $th;
+}
