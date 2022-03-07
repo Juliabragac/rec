53cddfe004770b62598ea39c9e8640cc969eeda5
Julia Braga <bragajuliacorr1@gmail.com>
2022-03-07 17:35:48 -0300

    mudança

--- php/conexao.php
+++ php/conexao.php
@@ -1,13 +1,25 @@
 <?php
 
-try {
-    $pdo = new PDO("mysql:dbname=loja;host=localhost","root", "");
+define("__HOST__", "localhost");
+define("__USER__", "root");
+define("__PASS__", "");
+define("__DATABASE__", "class");
 
-} catch (PDOException $th) {
-    echo "Erro com o banco de dados: " . $th;
-}
-catch (Exception $th) {
-    echo "Erro com o banco de dados: " . $th;
+function getConnection()
+{
+    try {
+        $key = 'strval';
+        $con = new PDO("mysql:host={$key(__HOST__)}; dbname={$key(__DATABASE__)}", __USER__, __PASS__)
+            or die("Error when trying to connect to the database");
+        $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        return $con;
+    } catch (PDOException $error) {
+        echo "Error when connect to database. Erro: {$error->getMessage()}";
+   
+exit;
+    }
 }
 
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+getConnection();
+
